2dcb66d07547f890e3dacecdbe0be9394d064e0f
rogerdpack <rogerpack2005@gmail.com>
2011-08-03 11:20:01 -0600

    splitify

--- acam/acam.h
+++ acam/acam.h
@@ -11,6 +11,11 @@
 #endif
 
 
+#include <streams.h>
+#include <initguid.h>
+#include <dllsetup.h>
+#include <stdio.h>
+
 // This class is exported from the acam.dll
 class ACAM_API Cacam {
 public:
@@ -21,3 +26,81 @@ public:
 extern ACAM_API int nacam;
 
 ACAM_API int fnacam(void);
+
+
+EXTERN_C const GUID CLSID_VirtualCam;
+
+class CVCamStream;
+class CVCam : public CSource
+{
+public:
+    //////////////////////////////////////////////////////////////////////////
+    //  IUnknown
+    //////////////////////////////////////////////////////////////////////////
+    static CUnknown * WINAPI CreateInstance(LPUNKNOWN lpunk, HRESULT *phr);
+    STDMETHODIMP QueryInterface(REFIID riid, void **ppv);
+
+    IFilterGraph *GetGraph() {return m_pGraph;}
+
+private:
+    CVCam(LPUNKNOWN lpunk, HRESULT *phr);
+};
+
+class CVCamStream : public CSourceStream, public IAMStreamConfig, public IKsPropertySet
+{
+public:
+
+    //////////////////////////////////////////////////////////////////////////
+    //  IUnknown
+    //////////////////////////////////////////////////////////////////////////
+    STDMETHODIMP QueryInterface(REFIID riid, void **ppv);
+    STDMETHODIMP_(ULONG) AddRef() { return GetOwner()->AddRef(); }                                                          \
+    STDMETHODIMP_(ULONG) Release() { return GetOwner()->Release(); }
+
+    //////////////////////////////////////////////////////////////////////////
+    //  IQualityControl
+    //////////////////////////////////////////////////////////////////////////
+    STDMETHODIMP Notify(IBaseFilter * pSender, Quality q);
+
+    //////////////////////////////////////////////////////////////////////////
+    //  IAMStreamConfig
+    //////////////////////////////////////////////////////////////////////////
+    HRESULT STDMETHODCALLTYPE SetFormat(AM_MEDIA_TYPE *pmt);
+    HRESULT STDMETHODCALLTYPE GetFormat(AM_MEDIA_TYPE **ppmt);
+    HRESULT STDMETHODCALLTYPE GetNumberOfCapabilities(int *piCount, int *piSize);
+    HRESULT STDMETHODCALLTYPE GetStreamCaps(int iIndex, AM_MEDIA_TYPE **pmt, BYTE *pSCC);
+
+    //////////////////////////////////////////////////////////////////////////
+    //  IKsPropertySet
+    //////////////////////////////////////////////////////////////////////////
+    HRESULT STDMETHODCALLTYPE Set(REFGUID guidPropSet, DWORD dwID, void *pInstanceData, DWORD cbInstanceData, void *pPropData, DWORD cbPropData);
+    HRESULT STDMETHODCALLTYPE Get(REFGUID guidPropSet, DWORD dwPropID, void *pInstanceData,DWORD cbInstanceData, void *pPropData, DWORD cbPropData, DWORD *pcbReturned);
+    HRESULT STDMETHODCALLTYPE QuerySupported(REFGUID guidPropSet, DWORD dwPropID, DWORD *pTypeSupport);
+    
+    //////////////////////////////////////////////////////////////////////////
+    //  CSourceStream
+    //////////////////////////////////////////////////////////////////////////
+    CVCamStream(HRESULT *phr, CVCam *pParent, LPCWSTR pPinName);
+    ~CVCamStream();
+
+    HRESULT FillBuffer(IMediaSample *pms);
+    HRESULT DecideBufferSize(IMemAllocator *pIMemAlloc, ALLOCATOR_PROPERTIES *pProperties);
+    HRESULT CheckMediaType(const CMediaType *pMediaType);
+    HRESULT GetMediaType(int iPosition, CMediaType *pmt);
+    HRESULT SetMediaType(const CMediaType *pmt);
+    HRESULT OnThreadCreate(void);
+    
+private:
+    CVCam *m_pParent;
+    // unused now? REFERENCE_TIME m_rtLastTime;
+    HBITMAP m_hLogoBmp;
+    CCritSec m_cSharedState;
+    IReferenceClock *m_pClock;
+    bool m_fFirstSampleDelivered;
+
+    CRefTime     m_rtSampleTime;    // The time to be stamped on each sample
+    LONGLONG m_llSampleMediaTimeStart;
+
+	HRESULT setAsNormal(CMediaType *pmt);
+
+};
