7b1c438f0d71eb745ed50ff6a333f3b55b3ff27f
taqattack <taqveer@gmail.com>
2012-10-03 01:34:43 -0300

    Update source_code/acam/loopback-capture-helpers.cpp
    Modified for getting microphone

--- source_code/acam/loopback-capture-helpers.cpp
+++ source_code/acam/loopback-capture-helpers.cpp
@@ -6,12 +6,95 @@
 #include <audioclient.h>
 #include <stdio.h>
 #include <avrt.h>
+#include <Functiondiscoverykeys_devpkey.h>
+#include "common.h"
+
+int read_config_setting(LPCTSTR szValueName, int default);
+
+HRESULT get_mic(IMMDevice **ppMMDevice) {
+
+    HRESULT hr = S_OK;
+	int micIndex = (int) read_config_setting(L"MicId",-1);
+	if (micIndex == -1)
+		micIndex = 1;
+
+    IMMDeviceEnumerator *pMMDeviceEnumerator;
+	// activate a device enumerator
+	hr = CoCreateInstance(
+		__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, 
+		__uuidof(IMMDeviceEnumerator),
+		(void**)&pMMDeviceEnumerator
+	);
+	if (FAILED(hr)) {
+		printf("CoCreateInstance(IMMDeviceEnumerator) failed: hr = 0x%08x\n", hr);
+		return hr;
+	}
+
+	if (micIndex == 1 || micIndex == 0)		//micIndex 0 should never happen but just in case
+	{
+		// get the default render endpoint
+		hr = pMMDeviceEnumerator->GetDefaultAudioEndpoint(eCapture, eConsole, ppMMDevice);
+		pMMDeviceEnumerator->Release();
+		if (FAILED(hr)) {
+			printf("IMMDeviceEnumerator::GetDefaultAudioEndpoint failed: hr = 0x%08x\n", hr);
+			return hr;
+		}
+
+		return S_OK;
+	}
+	else
+	{
+		int realIndex = micIndex - 2;	//Offset it for actual endpoint index
+		IMMDeviceCollection *pMMDeviceCollection;
+
+		// get all the active render endpoints
+		hr = pMMDeviceEnumerator->EnumAudioEndpoints(
+			eCapture, DEVICE_STATE_ACTIVE, &pMMDeviceCollection
+		);
+
+		UINT count;
+		hr = pMMDeviceCollection->GetCount(&count);
+		if (FAILED(hr)) {
+			pMMDeviceCollection->Release();
+			printf("IMMDeviceCollection::GetCount failed: hr = 0x%08x\n", hr);
+			return hr;
+		}
+
+		for (UINT i = 0; i < count; i++) {
+			IMMDevice *pMMDevice;
+
+			// get the "n"th device
+			hr = pMMDeviceCollection->Item(i, &pMMDevice);
+			if (FAILED(hr)) {
+				pMMDeviceCollection->Release();
+				printf("IMMDeviceCollection::Item failed: hr = 0x%08x\n", hr);
+				return hr;
+			}
+
+			if (i == realIndex)
+			{
+				hr = pMMDeviceCollection->Item(i, ppMMDevice);
+				if (FAILED(hr)) {
+					printf("IMMDeviceEnumerator::GetDefaultAudioEndpoint failed: hr = 0x%08x\n", hr);
+					return hr;
+				}
+			}
+		}
+		pMMDeviceEnumerator->Release();
+		if (FAILED(hr)) {
+			printf("IMMDeviceEnumerator::GetDefaultAudioEndpoint failed: hr = 0x%08x\n", hr);
+			return hr;
+		}
+		pMMDeviceCollection->Release();
+		return S_OK;
+	}
+}
 
 // I guess this is the default umm...audio output device?
-
 HRESULT get_default_device(IMMDevice **ppMMDevice) {
     HRESULT hr = S_OK;
     IMMDeviceEnumerator *pMMDeviceEnumerator;
+
     // activate a device enumerator
     hr = CoCreateInstance(
         __uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, 
@@ -25,7 +108,7 @@ HRESULT get_default_device(IMMDevice **ppMMDevice) {
 
     // get the default render endpoint
     hr = pMMDeviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, ppMMDevice);
-    pMMDeviceEnumerator->Release();
+	pMMDeviceEnumerator->Release();
     if (FAILED(hr)) {
         printf("IMMDeviceEnumerator::GetDefaultAudioEndpoint failed: hr = 0x%08x\n", hr);
         return hr;
